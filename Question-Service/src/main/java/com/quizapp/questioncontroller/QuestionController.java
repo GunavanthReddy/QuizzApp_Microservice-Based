f778795a9cb3e9af35fc3352a2b3d53cbf970679
Gunavanth Reddy <saigunavanth@gmail.com>
2025-05-04 18:03:12 -0400

    Added getquestions by ID

--- Question-Service/src/main/java/com/quizapp/questioncontroller/QuestionController.java
+++ Question-Service/src/main/java/com/quizapp/questioncontroller/QuestionController.java
@@ -77,4 +77,10 @@ public class QuestionController {
 		return questionService.getScore(responses);
 	}
 
+/*	@GetMapping("getquestion/{id}")
+	public ResponseEntity<Question> getQuestionById(@PathVariable int id)
+	{
+		return questionService.getQuestionById(id);
+	}*/
+
 }
